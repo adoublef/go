b70fd42cb92dd0e69fb692122622365af0a9dbf4
Rahim <kristopherab@gmail.com>
2025-03-02 09:52:18 +0000

    add router chaining helper and method override HOF

---
+++ net/http/httputil/method_override.go
@@ -0,0 +1,33 @@
+// Copyright 2025 Kristopher Rahim Afful-Brown. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package httputil
+
+import (
+	"net/http"
+)
+
+// MethodOverride is a higher order [http.handlerFunc] which allows the client
+// to override a POST request with a PUT, PATCH or DELETE.
+//
+// A search query ("_method") can be used or header ("X-HTTP-Method-Override").
+func MethodOverride(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			method := r.URL.Query().Get("_method")
+			if method == "" {
+				method = r.Header.Get("X-HTTP-Method-Override")
+			}
+			if method == http.MethodPut || method == http.MethodPatch || method == http.MethodDelete {
+				r2 := r.Clone(r.Context())
+				r2.Method = method
+				h.ServeHTTP(w, r2)
+				return
+			}
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
---
+++ net/http/httputil/method_override_test.go
@@ -0,0 +1,48 @@
+// Copyright 2025 Kristopher Rahim Afful-Brown. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package httputil_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "go.adoublef.dev/net/http/httputil"
+	"go.adoublef.dev/testing/is"
+)
+
+func Test_MethodOverride(t *testing.T) {
+	t.Run("Query", func(t *testing.T) {
+		mux := http.NewServeMux()
+		mux.HandleFunc("DELETE /{$}", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		s := httptest.NewServer(MethodOverride(mux))
+		t.Cleanup(func() { s.Close() })
+
+		rs, err := s.Client().Post(s.URL+"?_method=DELETE", "", nil)
+		is.OK(t, err) // (http.Client).Post
+		is.Equal(t, rs.StatusCode, http.StatusOK)
+	})
+
+	t.Run("Header", func(t *testing.T) {
+		mux := http.NewServeMux()
+		mux.HandleFunc("DELETE /{$}", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
+		s := httptest.NewServer(MethodOverride(mux))
+		t.Cleanup(func() { s.Close() })
+
+		r, err := http.NewRequest(http.MethodPost, s.URL, nil)
+		is.OK(t, err) // http.NewRequest
+
+		r.Header.Set("X-HTTP-Method-Override", http.MethodDelete)
+
+		rs, err := s.Client().Do(r)
+		is.OK(t, err) // (http.Client).Post
+		is.Equal(t, rs.StatusCode, http.StatusOK)
+	})
+}

8b1354bfa57aaeec456beda43faaa44197e8dcf0
Rahim <kristopherab@gmail.com>
2025-03-02 09:47:41 +0000

    add wait strat helper

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go.adoublef.dev
 
 go 1.24.0
 
-require github.com/matryer/is v1.4.1
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0
+	github.com/matryer/is v1.4.1
+)
---
+++ testing/wait/for_http.go
@@ -0,0 +1,41 @@
+// Copyright 2025 Kristopher Rahim Afful-Brown. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wait
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+)
+
+// ForHTTP
+func ForHTTP(ctx context.Context, timeout time.Duration, endpoint string, opts ...func(*http.Request)) error {
+	c := &http.Client{}
+	o := func() error {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, endpoint, nil)
+		if err != nil {
+			return fmt.Errorf("failed to create request: %w", err)
+		}
+		for _, o := range opts {
+			o(req)
+		}
+		res, err := c.Do(req)
+		if err != nil {
+			return fmt.Errorf("failed making request: %w", err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			return &backoff.PermanentError{Err: fmt.Errorf("not ready")}
+		}
+		return nil
+
+	}
+	bo := backoff.NewExponentialBackOff(backoff.WithMaxElapsedTime(timeout))
+	return backoff.Retry(o, backoff.WithContext(bo, ctx))
+}

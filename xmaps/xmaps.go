1af70fd35cae9b932b2390ad5747aa990704b174
Rahim <kristopherab@gmail.com>
2025-03-18 20:29:07 +0000

    update docs for xmaps

--- xmaps/xmaps.go
+++ xmaps/xmaps.go
@@ -1,3 +1,9 @@
+// Copyright 2025 Kristopher Rahim Afful-Brown. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package xmaps defines various functions useful with maps of any type.
 package xmaps
 
 import (
@@ -6,6 +12,7 @@ import (
 	"go.adoublef.dev/xiter"
 )
 
+// GroupFunc groups slice elements into a map of slices using a key function.
 func GroupFunc[K comparable, Slice ~[]V, V any](aa Slice, f func(V) K) map[K]Slice {
 	return xiter.Reduce(func(m map[K]Slice, a V) map[K]Slice {
 		k := f(a)
@@ -14,6 +21,7 @@ func GroupFunc[K comparable, Slice ~[]V, V any](aa Slice, f func(V) K) map[K]Sli
 	}, make(map[K]Slice), slices.Values(aa))
 }
 
+// SetFunc creates a set (as a map to empty structs) from a slice using a key function.
 func SetFunc[K comparable, Slice ~[]V, V any](aa Slice, f func(V) K) map[K]struct{} {
 	return xiter.Reduce(func(m map[K]struct{}, a V) map[K]struct{} {
 		k := f(a)

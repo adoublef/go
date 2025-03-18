dbf3e2bd24ce5c125046b29fa77bdfc140fb4de5
Rahim <kristopherab@gmail.com>
2025-03-18 20:35:58 +0000

    update docs within testing

--- testing/is/is.go
+++ testing/is/is.go
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package is provides a lightweight extension to the standard library's testing capabilities.
 package is
 
 import (

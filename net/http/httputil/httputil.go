42ea6434afb7e917c5e08dbe5c9df95c7ab6c5d0
Rahim <kristopherab@gmail.com>
2025-04-05 11:42:53 +0100

    add httputil consts

---
+++ net/http/httputil/httputil.go
@@ -0,0 +1,16 @@
+// Copyright 2025 Kristopher Rahim Afful-Brown. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package httputil
+
+import "time"
+
+const (
+	DefaultReadTimeout    = 100 * time.Second // Cloudflare's default read request timeout of 100s
+	DefaultWriteTimeout   = 30 * time.Second  // Cloudflare's default write request timeout of 30s
+	DefaultIdleTimeout    = 900 * time.Second // Cloudflare's default write request timeout of 900s
+	DefaultMaxHeaderBytes = 32 * (1 << 10)
+	DefaultMaxBytes       = 1 << 20 // Cloudflare's free tier limits of 100mb
+)
